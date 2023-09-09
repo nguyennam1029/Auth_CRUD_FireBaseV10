7f15f4e53f9b58302c23d90c13b45f61f62e7d8a
Văn Nam Nguyễn <nguyennam1029@gmail.com>
2023-09-09 13:48:38 +0700

    update

--- app.js
+++ app.js
@@ -1,109 +1,42 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import {
-  getDatabase,
-  set,
-  ref,
-  push,
-  onValue,
-  update,
-  remove,
-} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  setPersistence,
-  signInWithEmailAndPassword,
-  browserSessionPersistence,
+  auth,
   onAuthStateChanged,
   signOut,
-} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDb8mWUOBFOc0VcbuPOatjXFnKMVU2XpG0",
-  authDomain: "fir-crud-81dd0.firebaseapp.com",
-  databaseURL: "https://fir-crud-81dd0-default-rtdb.firebaseio.com",
-  projectId: "fir-crud-81dd0",
-  storageBucket: "fir-crud-81dd0.appspot.com",
-  messagingSenderId: "330017261587",
-  appId: "1:330017261587:web:13d7dacda9603ac9a5d7e6",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const auth = getAuth();
-function getUserForm(e) {
-  e.preventDefault();
-  const name = userForm.name.value;
-  const email = userForm.email.value;
-  const password = userForm.password.value;
-  addData(name, email, password);
-}
-function addData(name, email, password) {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      set(ref(db, "users/" + user.uid), {
-        name: name,
-        email: email,
-        password: password,
-      });
-      alert("Created");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
-}
-
-const userForm = document.getElementById("form-add-user");
-userForm.addEventListener("submit", getUserForm);
-
-function getLoginForm(e) {
-  e.preventDefault();
-
-  const email = loginForm.email.value;
-  const password = loginForm.password.value;
-  setPersistence(auth, browserSessionPersistence);
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      alert("Signed");
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-}
-
-const loginForm = document.getElementById("form-login");
-loginForm.addEventListener("submit", getLoginForm);
-
+  
+} from "./fireBase-auth.js";
+// ====================HANDLE AUTH=======================
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    // ...
-    console.log(user);
+    const uid = user.uid;
+    const createButton = document.getElementById("createButton");
+
+    const authButtons = document.getElementById("authButtons");
+
+    if (uid) {
+      createButton.classList.add("block");
+      authButtons.classList.add("hidden");
+      authButtons.classList.remove("block");
+    }
+    const btnSignOut = document.getElementById("logOut");
+
+    async function handleLogOut() {
+      try {
+        alert("Bạn có muốn đăng xuất tài khoản không ?");
+        await signOut(auth);
+      } catch (error) {
+        console.log("🚀 ~ file: index.html:177 ~ handleLogOut ~ error:", error);
+      }
+    }
+    btnSignOut.addEventListener("click", handleLogOut);
   } else {
-    // User is signed out
-    // ...
+    const createButton = document.getElementById("createButton");
+
+    const authButtons = document.getElementById("authButtons");
+    createButton.classList.add("hidden");
+    authButtons.classList.add("block");
+    authButtons.classList.remove("hidden");
   }
 });
 
-const btnOut = document.getElementById("log-out");
-btnOut.addEventListener("click", handleLogOut);
+// ==========================RENDER POSTS===================
 
-function handleLogOut() {
-  signOut(auth)
-    .then(() => {
-      // Sign-out successful.
-      alert("User Log out");
-    })
-    .catch((error) => {
-      // An error happened.
-    });
-}
